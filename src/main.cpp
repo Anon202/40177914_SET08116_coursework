f2b3bb57566a5d90d8dea58c1e3ac2f49bda62e4
Fionn Mahnig <fionnmahnig@gmail.com>
2017-02-19 17:33:27 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,82 @@
+#include <glm\glm.hpp>
+#include <graphics_framework.h>
+
+using namespace std;
+using namespace graphics_framework;
+using namespace glm;
+
+map<string, mesh> meshes;
+geometry geom;
+effect eff;
+target_camera cam;
+texture tex;
+
+bool load_content() {
+  // Create triangle data
+  // Create plane mesh
+	meshes["plane"] = mesh(geometry_builder::create_plane());
+	// Create scene
+	meshes["box"] = mesh(geometry_builder::create_box());
+	// Transform objects
+	meshes["box"].get_transform().scale = vec3(5.0f, 5.0f, 5.0f);
+	meshes["box"].get_transform().translate(vec3(-10.0f, 2.5f, -30.0f));
+
+	// Load texture
+	tex = texture("textures/checker.png");
+
+  // Load in shaders
+  eff.add_shader("shaders/simple_texture.vert", GL_VERTEX_SHADER);
+  eff.add_shader("shaders/simple_texture.frag", GL_FRAGMENT_SHADER);
+  // Build effect
+  eff.build();
+
+  // Set camera properties
+  cam.set_position(vec3(50.0f, 10.0f, 50.0f));
+  cam.set_target(vec3(0.0f, 0.0f, 0.0f));
+  cam.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
+  return true;
+}
+
+
+bool update(float delta_time) {
+  // Update the camera
+  cam.update(delta_time);
+  return true;
+}
+
+bool render() {
+	for (auto &e : meshes) {
+		auto m = e.second;
+		// Bind effect
+		renderer::bind(eff);
+		// Create MVP matrix
+		auto M = m.get_transform().get_transform_matrix();
+		auto V = cam.get_view();
+		auto P = cam.get_projection();
+		auto MVP = P * V * M;
+		// Set MVP matrix uniform
+		glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+
+		// Bind texture to renderer
+		renderer::bind(tex, 0);
+		// Set the texture value for the shader here
+		glUniform1i(eff.get_uniform_location("tex"), 0);
+
+		// Render mesh
+		renderer::render(m);
+	}
+  // Render geometry
+  //renderer::render(geom);
+  return true;
+}
+
+void main() {
+  // Create application
+  app application("Graphics Coursework");
+  // Set load content, update and render methods
+  application.set_load_content(load_content);
+  application.set_update(update);
+  application.set_render(render);
+  // Run application
+  application.run();
+}
